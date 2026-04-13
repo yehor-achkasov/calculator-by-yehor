94bf8a2a744c1086b9246bc0886bebe3657c2aa8
Yehor <achkasov.yehor@gmail.com>
2026-04-13 09:53:35 +0000

    the interface has been changed, a "C" button has been added

--- calculator.py
+++ calculator.py
@@ -1,68 +1,44 @@
-import tkinter as tk                                                    # import tkinter
+import tkinter as tk      # import tkinter
 
 root = tk.Tk()
 root.title("calculator")
-root.geometry("900x450")                                                # create a window
+root.geometry("300x250")  # create a window
 
-entry = tk.Entry(root, width=20)
-entry.pack()                                                            # add entry line 
+entry=tk.Entry(root, width=30)
+entry.grid(row=0, column=0, columnspan=4)          # add entry
 
 def add_symbol(symbol):
     entry.insert(tk.END, symbol) 
 
-button1 = tk.Button(root, text='1', command=lambda: add_symbol('1')) 
-button1.pack()  
-           
-button2 = tk.Button(root, text="2", command=lambda: add_symbol("2"))
-button2.pack()
-
-button3 = tk.Button(root, text="3", command=lambda: add_symbol("3"))
-button3.pack()
-
-button4 = tk.Button(root, text="4", command=lambda: add_symbol("4"))
-button4.pack()
-
-button5 = tk.Button(root, text="5", command=lambda: add_symbol("5"))
-button5.pack()
-
-button6 = tk.Button(root, text="6", command=lambda: add_symbol("6"))
-button6.pack()
-
-button7 = tk.Button(root, text='7', command=lambda: add_symbol('7'))
-button7.pack()
-
-button8 = tk.Button(root, text='8', command=lambda: add_symbol('8'))
-button8.pack()
-
-button9 = tk.Button(root, text="9", command=lambda: add_symbol('9'))
-button9.pack()
-
-button0 = tk.Button(root, text='0', command=lambda: add_symbol('0'))
-button0.pack()                                                          #add defaulte symbols 
-
-
-
-button_plus = tk.Button(root, text="+", command=lambda: add_symbol("+"))
-button_plus.pack()
-
-button_minus = tk.Button(root, text="-", command=lambda: add_symbol("-"))
-button_minus.pack()
-
-button_mul = tk.Button(root, text="*", command=lambda: add_symbol("*"))
-button_mul.pack()
-
-button_div = tk.Button(root, text="/", command=lambda: add_symbol("/"))
-button_div.pack()                                                        #add operators                                                       
 
 def calculate():
     expression = entry.get()
     result = eval(expression)
     entry.delete(0, tk.END)
     entry.insert(0, result) 
-    
-button_equal = tk.Button(root, text="=", command=calculate)
-button_equal.pack()                                                      #add '='
-  
+
+
+tk.Button(root, text="7", command=lambda: add_symbol("7")).grid(row=1, column=0)
+tk.Button(root, text="8", command=lambda: add_symbol("8")).grid(row=1, column=1)
+tk.Button(root, text="9", command=lambda: add_symbol("9")).grid(row=1, column=2)
+tk.Button(root, text="/", command=lambda: add_symbol("/")).grid(row=1, column=3)
+tk.Button(root, text='4', command=lambda: add_symbol('4')).grid(row=2, column=0)
+tk.Button(root, text='5', command=lambda: add_symbol('5')).grid(row=2, column=1)
+tk.Button(root, text='6', command=lambda: add_symbol('6')).grid(row=2, column=2)
+tk.Button(root, text='*', command=lambda: add_symbol('*')).grid(row=2, column=3)
+tk.Button(root, text='1', command=lambda: add_symbol('1')).grid(row=3, column=0)
+tk.Button(root, text='2', command=lambda: add_symbol('2')).grid(row=3, column=1)
+tk.Button(root, text='3', command=lambda: add_symbol('3')).grid(row=3, column=2)
+tk.Button(root, text='+', command=lambda: add_symbol('+')).grid(row=3, column=3)
+tk.Button(root, text='0', command=lambda: add_symbol('0')).grid(row=4, column=0)
+tk.Button(root, text='-', command=lambda: add_symbol('-')).grid(row=4, column=1)
+tk.Button(root, text='=', command=calculate).grid(row=4, column=2)
+
+def clear():
+    entry.delete(0, tk.END) 
+
+tk.Button(root, text='C', command=clear).grid(row=4, column=3)
+
 
 
 
